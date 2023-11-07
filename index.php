70bad7ac8a71b4c0f46ef17beb6f6c07fbab3613
RafiJurnalis10 <150123688+RafiJurnalis10@users.noreply.github.com>
2023-11-07 15:40:30 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$jawabanIsset = "Fungsi isset pada PHP adalah Fungsi isset () digunakan untuk memeriksa apakah suatu variabel sudah diatur atau belum. Fungsi isset () mengembalikan false jika variabel pengujian berisi nilai NULL.
+
+Fungsi isset () akan menghasilkan nilai true jika sebuah variabel telah didefenisikan, dan false jika variabel tersebut belum dibuat. Fungsi isset() biasanya digunakan untuk mengecek sebuah inputan entah itu teksfield, checkbox, atau radiobutton.
+
+Jika beberapa parameter diberikan, maka isset () akan mengembalikan TRUE hanya jika semua parameter dianggap ditetapkan. Evaluasi beralih dari kiri ke kanan dan berhenti segera setelah variabel yang tidak disetel dijumpai.";
+function jawaban_isset(){
+	$jawabanIsset;
+	echo $jawabanIsset . '<br>';
+}
+
+
+
+
+ ?>
